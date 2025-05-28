8bfe1b95a2698f1c88135ee3b3da16a2ed25a353
giorgishevliakovi <gshevliakovi@gmail.com>
2025-05-28 19:36:23 +0400

    first commit

---
+++ day11/classwork/classwork.py
@@ -0,0 +1,22 @@
+for i in range(1, 11):
+    print(i)
+
+
+for num in range(1,5):
+    print(num)
+
+for i in range(2, 21, 2):
+    print(i)
+
+
+for i in range(10, 0, -1):
+    print(i)
+
+i = 10
+while i > 0:
+    print(i)
+    i -= 1
+
+
+for i in range(0, 101 -3):
+    print(i)
